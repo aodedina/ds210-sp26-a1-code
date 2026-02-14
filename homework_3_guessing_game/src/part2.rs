ba800275a4a9ca9295d70fa638e0a4c3882c3620
aodedina <mide7@icloud.com>
2026-02-14 13:55:12 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -6,7 +6,20 @@ pub struct Part2 {}
 // Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        if min + 1 == max {
+            return min;
+        }
+        let middle_num = (min + max) / 2;
+        let solution = player.ask_to_compare(middle_num);
+        if solution == 0 {
+            return middle_num;
+        }
+        else if solution == -1 {
+            return Part2::guess_the_number(player, min, middle_num);
+        }
+        else {
+            return Part2::guess_the_number(player, middle_num+1, max);
+
+        }
     }
 }
