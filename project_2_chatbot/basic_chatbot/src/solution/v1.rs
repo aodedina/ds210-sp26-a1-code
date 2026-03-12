f3d8377e480cf6a40972ed519f828791eb964335
cprechtl4 <cprechtl@bu.edu>
2026-03-12 17:07:42 -0400

    Implemented Chatbot V1 functionality on s2basic

--- project_2_chatbot/basic_chatbot/src/solution/v1.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v1.rs
@@ -17,11 +17,19 @@ impl ChatbotV1 {
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
 
+        let asynchronous_output = chat_session.add_message(message); //send users message to chat session
+        let output = asynchronous_output.await; //wait for LLM to finish generating response; await pauses any input that may come in
+
+        //handle success and error cases coming from the LLM's response
+        let output_text = match output {
+            Ok(text) => text,
+            Err(_) => String::from("Sorry, couldn't generate a response."), //fallback response
+        };
+        return output_text;
+
         // You need to add your code here
         // You must find a way to add the given message to the chat_session!
         // consider https://docs.rs/kalosm/0.4.0/kalosm/language/struct.Chat.html#method.add_message
         // Hint: make sure you transform/extract the response message as a **String**.
-
-        return String::from("Hello, I am not a bot (yet)!");
     }
 }
