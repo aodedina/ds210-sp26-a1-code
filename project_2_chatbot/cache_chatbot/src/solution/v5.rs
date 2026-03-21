9aa4844d7d3c24e322a387ecdee4516034b3919a
cprechtl4 <cprechtl@bu.edu>
2026-03-21 13:30:55 -0400

    v5 implementation of chat with user

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -18,36 +18,40 @@ impl ChatbotV5 {
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
         let filename = &format!("{}.txt", username);
-        let cached_chat = self.cache.get_chat(&username);
+        let cached_chat = self.cache.get_chat(&username); //attempt to retrieve user's chat from cache
 
+        //check if chat found in cache
         match cached_chat {
+            //conversation found in cache
             Some(chat) => {
                 println!("chat_with_user: {username} is in the cache! Nice!");
-                let response = chat(&message).await.unwrap();
-                let session = chat.session().unwrap();
-                file_library::save_chat_session_to_file(&filename, &session);
-                response
+                let response = chat(&message).await.unwrap(); //send user's message to chatbot; wait for response
+                let session = chat.session().unwrap();  //get current session from the chat
+                file_library::save_chat_session_to_file(&filename, &session);  //save updated chat session to file
+
+                response  //return chatbot response
             }
 
+            //conversation not found in cache
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
                 
                 let mut chat = self.model
                     .chat()
-                    .with_system_prompt("The assistant will act like a pirate");
+                    .with_system_prompt("The assistant will act like a pirate");  //create new chat session with assistant as pirate message prompt
 
+                //load previously saved session from file
                 if let Some(session) = file_library::load_chat_session_from_file(&filename) {
-                    chat = chat.with_session(session);
+                    chat = chat.with_session(session);  //restore the past conversation
                 }
 
-                let response = chat(&message).await.unwrap();
+                let response = chat(&message).await.unwrap();  
                 let session = chat.session().unwrap();
                 file_library::save_chat_session_to_file(&filename, &session);
-                self.cache.insert_chat(username.clone(), chat.clone());
+
+                self.cache.insert_chat(username.clone(), chat.clone());  //insert this chat into cache (now most recently used chat)
 
                 response
-
-            
             }
         }
     }
