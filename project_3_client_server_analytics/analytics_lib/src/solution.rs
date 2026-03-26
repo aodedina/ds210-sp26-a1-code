aa206b799ab5f5e7848051f057db2efc0b490921
cprechtl4 <cprechtl@bu.edu>
2026-03-26 12:30:21 -0400

    original implementation of filter_dataset

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,9 +2,38 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+
+//Returns a new Dataset containing only rows that satisfy the given condition   
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    //Checks if a single row meets the given condition
+    fn row_satisfies_condition(row: &Row, condition: &Condition, dataset: &Dataset) -> bool {
+        match condition {
+            Condition::Equal(col_name, value) => {
+                let col_index = dataset.columns()
+                .iter()
+                .position(|(name, _)| name == col_name)
+                .unwrap();
+
+                let row_value = &row.get_value(col_index);
+
+                row_value == &value  //Checks if a column equals a specified value and keeps all columns
+            }
+            _ => false, 
+        }
+    }
+    //Create a new empty dataset with the same columns to hold filtered rows
+    let mut new_dataset = Dataset::new(dataset.columns().clone());
+
+    for row in dataset.iter() {
+        if row_satisfies_condition(row, filter, dataset) {
+            new_dataset.add_row(row.clone());
+        }
+    }
+    new_dataset
 }
+
+
+
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
     todo!("Implement this!");
