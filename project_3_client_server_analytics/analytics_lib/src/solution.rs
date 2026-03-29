7238f6a1763ced97b521b1a97930dd78b7d94d49
cprechtl4 <cprechtl@bu.edu>
2026-03-29 08:56:59 -0400

    revisions to other test scenerios for filter_dataset function in order to pass the tests; revisions to group_by_dataset by implementing .into_iter in order to reduce unecessary cloning

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -5,7 +5,7 @@ use crate::query::{Aggregation, Condition, Query};
 
 //Returns a new Dataset containing only rows that satisfy the given condition   
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    //Checks if a single row meets the given condition
+    //Checks if a single row meets the given condition (Equal, Not, And, Or)
     fn row_satisfies_condition(row: &Row, condition: &Condition, dataset: &Dataset) -> bool {
         match condition {
             Condition::Equal(col_name, value) => {
@@ -17,8 +17,18 @@ pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
                 let row_value = &row.get_value(col_index);
 
                 row_value == &value  //Checks if a column equals a specified value and keeps all columns
+            } 
+            Condition::Not(inner) => {
+                !row_satisfies_condition(row, inner, dataset)
             }
-            _ => false, 
+            Condition::And(cond1, cond2) => {
+                row_satisfies_condition(row, cond1, dataset)
+                && row_satisfies_condition(row, cond2, dataset)
+            }
+            Condition::Or(cond1, cond2) => {
+                row_satisfies_condition(row, cond1, dataset)
+                || row_satisfies_condition(row, cond2, dataset)  
+            }
         }
     }
     //Create a new empty dataset with the same columns to hold filtered rows
@@ -36,12 +46,87 @@ pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
 
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
-    todo!("Implement this!");
+    let mut groups: HashMap<Value, Dataset> = HashMap::new();
+    //finds index of the column used for grouping
+    let column_index = dataset.column_index(group_by_column);
+    let columns = dataset.columns().clone();
+    //goes through each row
+    for row in dataset.into_iter() {
+        let key = row.get_value(column_index).clone();
+        //checks if group already exists
+        if groups.contains_key(&key) {
+            let data_for_group = groups.get_mut(&key).unwrap(); //gets group and adds row
+            data_for_group.add_row(row);
+        } else {
+            //creates new group dataset for else group
+            let mut new_data = Dataset::new(columns.clone());
+            new_data.add_row(row);
+            //inserts into the hashmap
+            groups.insert(key, new_data);
+        }
+
+    }
+    return groups;
 }
 
+
+//aggregates each group (Count, Sum, or Average) and returns a HashMap each group value to its aggregated result
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
-    todo!("Implement this!");
+    let mut result = HashMap::new();
+
+    //iterate over each group in the dataset; use into_iter to take ownership of the data and iterates over owned elements
+    for (group_value, group_dataset) in dataset.into_iter() {
+        //compute the aggregation for the current group
+        let agg_value = match aggregation {
+            //count number of rows in the group
+            Aggregation::Count(_) => {
+                Value::Integer(group_dataset.len() as i32)
+            }
+
+            //sum values in the specified column
+            Aggregation::Sum(column_name) => {
+                let mut sum = 0;
+                let col_index = group_dataset.columns()
+                    .iter()
+                    .position(|(name, _)| name == column_name)
+                    .unwrap();
+
+                for row in group_dataset.iter() {
+                    if let Value::Integer(v) = row.get_value(col_index) {
+                        sum += *v;
+                    }
+                }
+                Value::Integer(sum)
+            }
+             //compute average of the specified column
+            Aggregation::Average(column_name) => {
+                let mut sum = 0;
+                let mut count = 0;
+                let col_index = group_dataset.columns()
+                    .iter()
+                    .position(|(name, _)| name == column_name)
+                    .unwrap();
+
+                for row in group_dataset.iter() {
+                    if let Value::Integer(v) = row.get_value(col_index) {
+                        sum += *v;
+                        count += 1;
+                    }
+                }
+
+                let avg = if count > 0 {sum / count} else {0};
+                Value::Integer(avg)
+
+            }
+        };
+        //store the aggregated value for this group
+        result.insert(group_value, agg_value);
+    }
+    result
 }
+
+
+
 
 pub fn compute_query_on_dataset(dataset: &Dataset, query: &Query) -> Dataset {
     let filtered = filter_dataset(dataset, query.get_filter());
