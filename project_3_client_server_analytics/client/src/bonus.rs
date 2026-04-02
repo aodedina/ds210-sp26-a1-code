0754d2fdad829b16db2b205c6eccd3855d9a62ae
aodedina <mide7@icloud.com>
2026-04-02 16:24:42 -0400

    comments added for bonus section

--- project_3_client_server_analytics/client/src/bonus.rs
+++ project_3_client_server_analytics/client/src/bonus.rs
@@ -8,17 +8,20 @@ use client::{start_client, solution};
 
 // Your solution goes here.
 fn parse_query_from_string(input: String) -> Query {
+    //splits input string into parts using whitespace
     let parts: Vec<&str> = input.split_whitespace().collect();
 
     // fliter A1 section, gorup by grade and count name 
     let filter_column = parts[1].to_string();
 
+    //trims quotes from string and filter
     let filter_value = parts[3]
         .trim_matches('"')
         .to_string();
 
+    //column group by
     let group_by = parts[6].to_string();
-
+    //type of aggregation like count or sum and then column aggregation
     let aggregation_type = parts[7];
     let aggregation_column = parts[8].to_string();
 
@@ -26,14 +29,14 @@ fn parse_query_from_string(input: String) -> Query {
         filter_column,
         Value::String(filter_value),
     );
-
+    //determines aggregation type and creates aggregation object
     let aggregation = match aggregation_type {
         "COUNT" => Aggregation::Count(aggregation_column),
         "SUM" => Aggregation::Sum(aggregation_column),
         "AVERAGE" => Aggregation::Average(aggregation_column),
         _ => panic!("Invalid aggregation"),
     };
-
+    //returns query object using elements below
     Query::new(filter, group_by, aggregation)
 }
 
