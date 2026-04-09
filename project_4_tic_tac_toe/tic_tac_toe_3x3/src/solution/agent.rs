117d45a8eb64126390f75b6936f4a277e54e4830
cprechtl4 <cprechtl@bu.edu>
2026-04-09 15:33:29 -0400

    partner 2 initial implementation of solve

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -10,9 +10,63 @@ impl Agent for SolutionAgent {
     // Should returns (<score>, <x>, <y>)
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
+    // explores possible game states and chooses the optimal move; X maximizes score and O minimizes score 
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
         // If you want to make a recursive call to this solution, use
         // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+
+        //base case: game is over, return final score
+        if board.game_over() {
+            return (board.score(), 0,0);
+        }
+
+        //get all possible moves on the board
+        let moves = board.moves();
+
+        //start with move at index 1 being best, then iterates through all moves 
+        let mut best_move = moves[0];
+        //initialize best score. X wants to maximize so start with worst possible scenerio
+        // and move up; O wants to minimize so start with worst possible scenrio and
+        // move down
+        let mut best_score;
+        match player {
+            Player::X => best_score = i32::MIN,
+            Player::O => best_score = i32::MAX,
+        }
+
+        //loop through possible moves
+        for m in moves {
+            board.apply_move(m, player);
+
+            //evaluate the resulting board
+            let (score, _, _) = SolutionAgent::solve(
+                board, 
+                match player {
+                    Player::X => Player::O,
+                    Player::O => Player::X,
+                },
+                _time_limit,
+            );
+            //unmove board before trying other possible moves
+            board.undo_move(m, player);
+
+            //choose best move for player
+            match player {
+                Player::X => {
+                    if score > best_score {
+                        best_score = score;
+                        best_move = m;
+                    }
+                }
+                Player::O => {
+                    if score < best_score {
+                        best_score = score;
+                        best_move = m;
+                    }
+                }
+            }
+        }
+        let (x, y) = best_move;
+        return (best_score, x, y);
     }
 }
