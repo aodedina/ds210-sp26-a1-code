55d915159ef73083ee769fb8dad09d097443800d
aodedina <mide7@icloud.com>
2026-04-17 18:40:52 -0400

    final implementation with updated comments and fixed errors

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -6,7 +6,18 @@ pub struct SolutionAgent {}
 
 impl Agent for SolutionAgent {
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        let depth_lim = 4;
+        //counts amount of moves left
+        let moves_left = board.moves().len();
+        let depth_lim;
+        //deeper search for when board is more empty
+        if moves_left <= 5 {
+            depth_lim = 7;
+        } else if moves_left <= 9 {
+            depth_lim = 6;
+        } else {
+            depth_lim = 4;
+        }
+        //starts minimax algorithm
         SolutionAgent::minimax(board, player, 0, depth_lim)
     }
 }
@@ -18,20 +29,22 @@ impl SolutionAgent {
         depth: i32,
         max_depth: i32,
     ) -> (i32, usize, usize) {
+        
         if board.game_over() {
-            return (board.score(), 0, 0);
+            return (board.score() * 1000, 0, 0);
         }
-
-        if depth == max_depth {
+        //base case
+        if depth >= max_depth {
             return (Self::heuristic(board), 0, 0);
         }
-
+        //gets all moves
         let moves = board.moves();
-
+        //tracks best move and score
         let mut best_score;
         let mut best_row = 0;
         let mut best_column = 0;
 
+        //x wants to maximize, o wants to minimize
         if player == Player::X {
             best_score = -100000;
         } else {
@@ -40,14 +53,14 @@ impl SolutionAgent {
 
         for move_pos in moves {
             board.apply_move(move_pos, player);
-
+            //switch player
             let next_player;
             if player == Player::X {
                 next_player = Player::O;
             } else {
                 next_player = Player::X;
             }
-
+            //recursively evaluate the move
             let result = Self::minimax(
                 board,
                 next_player,
@@ -58,7 +71,7 @@ impl SolutionAgent {
             let score = result.0;
 
             board.undo_move(move_pos, player);
-
+            //update best move depending on who is playing
             if player == Player::X {
                 if score > best_score {
                     best_score = score;
@@ -73,11 +86,11 @@ impl SolutionAgent {
                 }
             }
         }
-
+        //returns best score and best moves
         (best_score, best_row, best_column)
     }
-
+    //estimates board value when depth lim is reached
     fn heuristic(board: &Board) -> i32 {
-        board.score() * 10
+        board.score() * 50
     }
 }
